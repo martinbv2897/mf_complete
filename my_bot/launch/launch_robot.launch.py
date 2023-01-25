d895fd404a6e4317fd4c9e285f8cffcb3021f6f5
martinbv <martinb2897@gmail.com>
2023-01-25 09:57:51 -0300

    ultimos ajustes

--- my_bot/launch/launch_robot.launch.py
+++ my_bot/launch/launch_robot.launch.py
@@ -9,7 +9,6 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command
 from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessStart
-
 from launch_ros.actions import Node
 
 
@@ -27,14 +26,16 @@ def generate_launch_description():
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
                 )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
     )
-
+    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    twist_mux = Node(
+            package="twist_mux",
+            executable="twist_mux",
+            parameters=[twist_mux_params],
+            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+        )
     
-
-
     robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
-
     controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
-
     controller_manager = Node(
         package="controller_manager",
         executable="ros2_control_node",
@@ -44,15 +45,6 @@ def generate_launch_description():
     # robot_localization_file_path = os.path.join(get_package_share_directory(package_name), 'config/ekf.yaml') 
     
     delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
-    
-    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
-    
-    twist_mux = Node(
-            package="twist_mux",
-            executable="twist_mux",
-            parameters=[twist_mux_params],
-            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
-        )
     diff_drive_spawner = Node(
         package="controller_manager",
         executable="spawner.py",
